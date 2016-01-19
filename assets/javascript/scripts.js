9c46ffc6ea930ff25158b0b08b8ccda329e27f0e
smellyalater <acopleman@gmail.com>
2016-01-19 00:03:36 -0500

    removed all bootstrap and added Milligram css

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-  console.log('page ready and js working');
+
+
+
+//js to toggle tab-content on mainpage
+    $('.tabs .tab-links a').click(function(e)  {
+        var currentAttrValue = $(this).attr('href');
+ 
+        // Show/Hide Tabs
+        $('.tabs ' + currentAttrValue).fadeIn(400).siblings().hide();
+ 
+        // Change/remove current tab to active
+        $(this).parent('li').addClass('active').siblings().removeClass('active');
+ 
+        e.preventDefault();
+    });
+
+
 });
